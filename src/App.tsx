130adc04a62a3c4bcebc220afb64844c732dcc30
Adelkilde <William.adelkilde@gmail.com>
2024-02-28 08:57:42 +0100

    Very good?

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,13 @@ import { useState } from "react";
 import "./App.css";
 import PropsDemo from "./exercises/PropsDemo";
 import ListDemo from "./exercises/ListDemo";
+import EventDemo from "./exercises/EventDemo";
+import FormUncrontrolled from "./exercises/FormUncontrolled";
+import StateDemo from "./exercises/StateDemo1";
+import StateDemo2 from "./exercises/StateDemo2";
+import StateDemo3 from "./exercises/StateDemo3";
+import UseEffectDemo from "./exercises/UseEffect";
+import FetchDemo from "./exercises/FetchDemo";
 
 export default function App() {
   const [selectedView, setSelectedView] = useState("info");
@@ -24,6 +31,13 @@ export default function App() {
             {selectedView == "info" ? <p>All exercises for React day-1</p> : null}
             {selectedView == "props1" ? <PropsDemo title="Props Demo 1" /> : null}
             {selectedView == "list1" ? <ListDemo title="List Demo 1" /> : null}
+            {selectedView == "event1" ? <EventDemo title="Event Demo 1" /> : null}
+            {selectedView == "form-uncontrolled" ? <FormUncrontrolled title="Form Demo 1" /> : null}
+            {selectedView == "state1" ? <StateDemo title="State Demo 1" /> : null}
+            {selectedView == "state2" ? <StateDemo2 title="State Demo 2" /> : null}
+            {selectedView == "state3" ? <StateDemo3 title="State Demo 3" /> : null}
+            {selectedView == "useEffect" ? <UseEffectDemo title="UseEffect Demo" /> : null}
+            {selectedView == "fetch" ? <FetchDemo title="Fetch Demo" /> : null}
             {/**Add the exercise components you create for each exercise using the key you used for the matching button  */}
           </div>
         </div>
@@ -47,6 +61,27 @@ const Buttons = (props: ButtonProps) => {
       </button>
       <button className="btn-w100" onClick={() => handleSelected("list1")}>
         List demo1
+      </button>
+      <button className="btn-w100" onClick={() => handleSelected("event1")}>
+        Event demo1
+      </button>
+      <button className="btn-w100" onClick={() => handleSelected("form-uncontrolled")}>
+        Forms Uncontrolled
+      </button>
+      <button className="btn-w100" onClick={() => handleSelected("state1")}>
+        State demo1
+      </button>
+      <button className="btn-w100" onClick={() => handleSelected("state2")}>
+        State demo2
+      </button>
+      <button className="btn-w100" onClick={() => handleSelected("state3")}>
+        State demo3
+      </button>
+      <button className="btn-w100" onClick={() => handleSelected("useEffect")}>
+        UseEffect demo
+      </button>
+      <button className="btn-w100" onClick={() => handleSelected("fetch")}>
+        Fetch demo
       </button>
     </>
   );
---
+++ src/components/UserForm2.tsx
@@ -0,0 +1,35 @@
+import { BaseProps } from "../types";
+import { FormEvent, useRef } from "react";
+import { User } from "../data/data";
+
+type UserFormProps = BaseProps & {
+  onSubmitUser: (user: User) => void;
+};
+
+export default function UserForm2({ title, onSubmitUser }: UserFormProps) {
+  const nameRef = useRef<HTMLInputElement>(null);
+  const emailRef = useRef<HTMLInputElement>(null);
+  const isActiveRef = useRef<HTMLInputElement>(null);
+
+  const onSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const newUser: User = {
+      name: nameRef.current?.value || "",
+      email: emailRef.current?.value || "",
+      isActive: isActiveRef.current?.checked || false,
+    };
+    onSubmitUser(newUser);
+  };
+
+  return (
+    <>
+      <h2>{title}</h2>
+      <form onSubmit={onSubmit}>
+        First Name: <input ref={nameRef} name="name" />
+        Email: <input ref={emailRef} name="email" />
+        Active: <input ref={isActiveRef} type="checkbox" name="isActive" />
+        <button type="submit">Add User</button>
+      </form>
+    </>
+  );
+}
